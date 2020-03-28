685ffaa8db735aca83ae6e6ceea08cb04bd59ec2
liaoyuhao <342129019@qq.com>
2020-03-27 23:34:17 -0230

    Finishing point 1

--- voting.m
+++ voting.m
@@ -1,5 +1,5 @@
 clear;
-oim1 = imread('img/01.jpg');
+oim1 = imread('img/03.jpg');
 oim2 = imread('img/ref.jpg');
 
 [row1,col1,n] = size(oim1);
@@ -17,24 +17,29 @@ end
 [fim2,dim2] = vl_sift(single(oim2));
 [matches,scores] = vl_ubcmatch(dim1,dim2);
 
-im1_Points = fim1(:,matches(1,:));
-im2_Points = fim2(:,matches(2,:));
+[sv, indx] = sort(scores, 'ascend');
+match_offs = matches(:, indx);
+offs1 = match_offs(1, 1:10);
+offs2 = match_offs(2, 1:10);
+
+im1_points = fim1(:,offs1);
+im2_points = fim2(:,offs2);
 
 figure;
-showMatchedFeatures(oim1, oim2, im1_Points(1:2,:)', ...
-    im2_Points(1:2,:)', 'montage');
+showMatchedFeatures(oim1, oim2, im1_points(1:2,:)', ...
+    im2_points(1:2,:)', 'montage');
 
 ref = [1;1];
-top_left = normalize(ref,im1_Points,im2_Points,row2,col2);
+top_left = normalize(ref,im1_points,im2_points,row2,col2);
 
 ref = [1;col1];
-top_right = normalize(ref,im1_Points,im2_Points,row2,col2);
+top_right = normalize(ref,im1_points,im2_points,row2,col2);
 
 ref = [row1;1];
-bottom_left = normalize(ref,im1_Points,im2_Points,row2,col2);
+bottom_left = normalize(ref,im1_points,im2_points,row2,col2);
 
 ref = [row1;col1];
-bottom_right = normalize(ref,im1_Points,im2_Points,row2,col2);
+bottom_right = normalize(ref,im1_points,im2_points,row2,col2);
 
 corners = [top_left;top_right;bottom_right;bottom_left];
 
@@ -42,10 +47,10 @@ corners_maxi = max(corners);
 [corners_mini,ind] = min(corners);
 dim = corners_maxi - corners_mini;
 
-%[r,c] = size(im1_Points);
+%[r,c] = size(im1_points);
 
-%row1 = (im2_Points(3,c)/im1_Points(3,c)).*row1;
-%col1 = (im2_Points(3,c)/im1_Points(3,c)).*col1;
+%row1 = (im2_points(3,c)/im1_points(3,c)).*row1;
+%col1 = (im2_points(3,c)/im1_points(3,c)).*col1;
 
 %row1 = row1*1.2;col1 = col1*1.2;
 
